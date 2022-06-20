0ea71f6a8fb20210fdbad39df23db037cac07a6d
Emeka Ike <emekagike@gmail.com>
2022-06-20 14:53:37 +0100

    I removed my security keys online

--- EmekaIke/EmekaIke/settings.py
+++ EmekaIke/EmekaIke/settings.py
@@ -19,11 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yqdo*e+uu%o^w8q+bcw+x=v078=p*)4tk694^k-@u*#tfvvc%l'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
